baf4d273824ad7520115fb654cb0ceec6b5fdeb4
Cloud-Baba <81719233+Cloud-Baba@users.noreply.github.com>
2021-04-01 04:41:19 +0530

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: G-T6Q94FTMPZ,
+        trackingId: `G-T6Q94FTMPZ`,
       },
     },
     `gatsby-plugin-sitemap`,
